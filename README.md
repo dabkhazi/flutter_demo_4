7a089240a888d0a2f291315eba2fad09bbd339c5
Dmitry Abkhazi <dabkhazi@gmail.com>
2022-04-06 21:14:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter-plain
+
+Minimum flutter template.
---
+++ android/app/src/main/kotlin/com/example/flutter_plain/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_plain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
